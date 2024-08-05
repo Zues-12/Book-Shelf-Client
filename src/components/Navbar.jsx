d219a29eb2da89c91a52198941bfb8d92efd3da3
Hamdan <159038698+Hamdan-Zaheer@users.noreply.github.com>
2024-08-05 23:20:32 +0500

    again path change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -89,7 +89,7 @@ const Navbar = ({toggleSidebar}) => {
               className="flex text-2xl font-semibold items-center justify-center "
             >
               <img
-                src="/public/10433049.png"
+                src="./10433049.png"
                 alt="logo"
                 className="h-10 me-4"
               />{" "}
